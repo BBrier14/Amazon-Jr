07ae64463f85f1e9a2e7335b76c3a5ec94c2f3d2
Benjamin Brier <ben.brier14@gmail.com>
2019-10-29 09:41:40 -0400

    connection works, products display

---
+++ bamazonCustomer.js
@@ -0,0 +1,32 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "JeepJKU2015",
+  database: "bamazon_DB"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    showProducts();
+  });
+
+function showProducts(){
+    connection.query("SELECT * FROM products", function(err, res){
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    })
+}
